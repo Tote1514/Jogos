bb70a0649c5b12d8cbf980c4f56dd51f02cee4cd
José <joseantoniocastrom5@gmail.com>
2022-11-12 07:26:59 -0300

    Adicionei novos arquivos

--- 02-anim.c
+++ 02-anim.c
@@ -1,5 +1,27 @@
 #include <SDL2/SDL.h>
 
+
+#define WINDOW_HEIGHT 500
+#define WINDOW_WIDTH 500
+
+int AUX_WaitEventTimeoutCount(SDL_Event* evt, int* ms){
+    static int aux;
+    Uint32 antes = SDL_GetTicks();
+    int evento = SDL_WaitEventTimeout(evt, *ms);
+    if (evento) {
+        *ms -= (SDL_GetTicks() - antes);
+        aux += (SDL_GetTicks() - antes);
+        if (*ms <  0) {
+            *ms = 0;
+        }
+    }else{
+        *ms  += aux;
+        aux = 0;
+    }
+    return evento;
+
+}
+
 int main (int argc, char* args[])
 {
     /* INICIALIZACAO */
@@ -7,7 +29,7 @@ int main (int argc, char* args[])
     SDL_Window* win = SDL_CreateWindow("Animacao Simples",
                          SDL_WINDOWPOS_UNDEFINED,
                          SDL_WINDOWPOS_UNDEFINED,
-                         500, 500, SDL_WINDOW_SHOWN
+                         WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN
                       );
     SDL_Renderer* ren = SDL_CreateRenderer(win, -1, 0);
 
@@ -16,8 +38,11 @@ int main (int argc, char* args[])
     SDL_Rect r = {250,0, 10,10};
     int z = 2;// variação y
     int w = 2;//variação do x
-    int acabou = 0;
-    while (acabou <= 3) {
+    int close_request = 0;
+    int espera = 100;
+    SDL_Event evt;
+
+    while (!close_request) {
       SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
       SDL_RenderClear(ren);
       SDL_SetRenderDrawColor(ren, 0, 0, 0, 0x00);
@@ -25,19 +50,62 @@ int main (int argc, char* args[])
       SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
       SDL_RenderFillRect(ren, &r);
       SDL_RenderPresent(ren);
-      SDL_Delay(100);
-      if(r.y >= 40 && r.x >= 290){
-        w = -2;
-      }else if( r.y >= 90 && r.x <= 250){
-         z = -2;
-      }else if(r.x<=200 && r.y <= 50){
-        w = 2;
-      }else if(r.x >=240 && r.y <=0){
-        z = 2;
-        acabou += 1;
+
+
+      int isevt = AUX_WaitEventTimeoutCount(&evt, &espera);
+      if (isevt) {
+        if (evt.type == SDL_QUIT) {
+          close_request = 1;
+        }else if (evt.type == SDL_KEYDOWN) {
+          switch (evt.key.keysym.sym) {
+            case  SDLK_RIGHT:
+              if (r.x > 250 && r.x <= 290 && r.y > 0 && r.y <= 40) {
+                w = 2;
+                z = 2;
+              }else if ( r.y > 40 && r.y <= 90 && r.x <= 290 && r.x > 250) {
+                w = 2;
+                z = -2;
+              }else if (r.x <= 240 && r.x > 200 && r.y <= 90 && r.y > 50) {
+                w = 2;
+                z = 2;
+              }else if (r.y <= 40 && r.y >0 && r.x <=240 && r.x > 200) {
+                w = 2;
+                z = -2;
+              }
+              break;
+            case SDLK_LEFT:
+              if (r.x > 250 && r.x <= 290 && r.y > 0 && r.y <= 40) {
+                z = -2;
+                w = -2;
+              }
+              else if ( r.y > 40 && r.y <= 90 && r.x <= 290 && r.x > 250) {
+                z = 2;
+                w = -2;
+              }else if (r.x <= 240 && r.x > 200 && r.y <= 90 && r.y > 50) {
+                z = -2;
+                w = -2;
+              }else if (r.y <= 40 && r.y >0 && r.x <=240 && r.x > 200) {
+                w = -2;
+                z = 2;
+              }
+              break;
+          }
+          r.y += z;
+          r.x += w;
+        }
+      } else {
+        if(r.y >= 40 && r.x >= 290){
+          w = -2;
+        }else if( r.y >= 90 && r.x <= 250){
+          z = -2;
+        }else if(r.x<=200 && r.y <= 50){
+          w = 2;
+        }else if(r.x >=240 && r.y <=0){
+          z = 2;
+        }
+        r.y += z;
+        r.x += w;
       }
-      r.y += z;
-      r.x += w;
     }
 
     /* FINALIZACAO */
--- programa.c
+++ programa.c
@@ -1,6 +1,6 @@
 #include <SDL2/SDL.h>
 
-#define WINDOW_HEIGHT 100
+#define WINDOW_HEIGHT 200
 #define WINDOW_WIDTH 200
 
 int AUX_WaitEventTimeoutCount(SDL_Event* evt, int* ms){
@@ -14,7 +14,7 @@ int AUX_WaitEventTimeoutCount(SDL_Event* evt, int* ms){
             *ms = 0;
         }
     }else{
-        *ms  += aux;
+        *ms += aux;
         aux = 0;
     }
     return evento;
@@ -33,16 +33,24 @@ int main (int argc, char* args[])
     SDL_Renderer* ren = SDL_CreateRenderer(win, -1, 0);
 
     /* EXECUÇÃO */
-    SDL_Rect r = { 40,20, 10,10 };
+    SDL_Rect r1 = { 40,20, 10,10 };
+    SDL_Rect r2 = { 100, 100, 10, 10};
+    SDL_Rect r3 = { 100, 50, 10, 10};
     SDL_Event evt;
     int close_request = 0;
     int espera = 500;
+    int dx =2; //deslocamento do x
+    int dy =2; //deslocamento do y
 
     while (!close_request) {
         SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
         SDL_RenderClear(ren);
         SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
-        SDL_RenderFillRect(ren, &r);
+        SDL_RenderFillRect(ren, &r1);
+        SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
+        SDL_RenderFillRect(ren, &r2);
+        SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
+        SDL_RenderFillRect(ren, &r3);
         SDL_RenderPresent(ren);
 
         int isevt = AUX_WaitEventTimeoutCount(&evt, &espera);
@@ -50,35 +58,50 @@ int main (int argc, char* args[])
             if (evt.type == SDL_KEYDOWN) {
                 switch (evt.key.keysym.sym) {
                     case SDLK_UP:
-                        if (r.y > 0) {
-                            r.y -= 5;
+                        if (r1.y > 0) {
+                            r1.y -= 5;
                         }
                         break;
                     case SDLK_DOWN:
-                        if (r.y < WINDOW_HEIGHT - r.h) {
-                            r.y += 5;
+                        if (r1.y < WINDOW_HEIGHT - r1.h) {
+                            r1.y += 5;
                         }
                         break;
                     case SDLK_LEFT:
-                        if (r.x > 0) {
-                            r.x -= 5;
+                        if (r1.x > 0) {
+                            r1.x -= 5;
                         }
                         break;
                     case SDLK_RIGHT:
-                        if (r.x < WINDOW_WIDTH - r.w) {
-                            r.x += 5;
+                        if (r1.x < WINDOW_WIDTH - r1.w) {
+                            r1.x += 5;
                         }
                         break;
                 }
             }else if (evt.type == SDL_QUIT) {
                 close_request = 1;
-            }
-        } else {
-            if (r.x <= WINDOW_WIDTH- r.w && r.y <= WINDOW_HEIGHT- r.h) {
-                r.x += 2;
-                r.y += 2;
 
+            }else if(evt.type == SDL_MOUSEMOTION){
+                if(evt.button.x < WINDOW_WIDTH -r3.w){
+                   r3.x = evt.button.x;
+                }
+                if( evt.button.y < WINDOW_HEIGHT - r3.h){
+                   r3.y = evt.button.y;
+                }
             }
+        }else {
+           if( r2.x >= WINDOW_WIDTH - r2.w){
+               dx = -2;
+           }else if(r2.x <= 0){
+               dx = 2;
+           }
+           if( r2.y >= WINDOW_HEIGHT - r2.h){
+               dy = -2;
+           }else if(r2.y <= 0){
+               dy = 2;
+           }
+            r2.x += dx;
+            r2.y += dy;
         }
     }
 
